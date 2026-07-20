83ca076b55a2594d7cd46d3ffe4f7a882bf71475
David Faden (revfad.com) <dfaden@gmail.com>
2026-07-20 12:23:19 +0900

    Initial version of Katana to Korean mapping

---
+++ kokatakana.js
@@ -0,0 +1,102 @@
+const KATAKANA = [
+  ["ア", "イ", "ウ", "エ", "オ"],
+  ["カ", "キ", "ク", "ケ", "コ"],
+  ["サ", "シ", "ス", "セ", "ソ"],
+  ["タ", "チ", "ツ", "テ", "ト"],
+  ["ナ", "ニ", "ヌ", "ネ", "ノ"],
+  ["ハ", "ヒ", "フ", "ヘ", "ホ"],
+  ["マ", "ミ", "ム", "メ", "モ"],
+  ["ヤ", "", "ユ", "", "ヨ"],
+  ["ラ", "リ", "ル", "レ", "ロ"],
+  ["ワ", "ヰ", "", "ヱ", "ヲ"],
+];
+
+const ROMAJI_VOWELS = ["a", "i", "u", "e", "o"];
+
+const ROMAJI_CONSONANTS = ["", "k", "s", "t", "n", "h", "m", "y", "r", "w"];
+
+const NG = "\u110b";
+
+const VOWEL_MAP = {
+  a: "ᅡ",
+  i: "ᅵ",
+  u: "ᅮ",
+  e: "ᅦ",
+  o: "ᅩ",
+};
+
+const CONSONANT_MAP = {
+  "": NG,
+  k: "ᄀ",
+  s: "ᄉ",
+  t: "ᄃ",
+  n: "ᄂ",
+  h: "ᄒ",
+  m: "ᄆ",
+  y: NG,
+  r: "ᄅ",
+  w: NG,
+};
+
+const Y_MAP = {
+  a: "ᅣ",
+  i: "",
+  u: "ᅲ",
+  e: "ᅨ",
+  o: "ᅭ",
+};
+
+const W_MAP = {
+  a: "ᅪ",
+  i: "ᅬ",
+  u: "ᅮ",
+  e: "ᅰ",
+  o: "ᅯ",
+};
+
+const INITIAL_CONSONANTS = "ᄀᄁᄂᄃᄄᄅᄆᄇᄈᄉᄊᄋᄌᄍᄎᄏᄐᄑᄒ";
+const VOWELS = "ᅡᅢᅣᅤᅥᅦᅧᅨᅩᅪᅫᅬᅭᅮᅯᅰᅱᅲᅳᅴᅵ";
+
+function combineSyllables(initial, medial) {
+  const initialCode = INITIAL_CONSONANTS.indexOf(initial);
+  const medialCode = VOWELS.indexOf(medial);
+  return String.fromCharCode(588 * initialCode + 28 * medialCode + 44032);
+}
+
+const parent = document.getElementById("kokatakana");
+const table = document.createElement("table");
+const vowelsTr = document.createElement("tr");
+vowelsTr.appendChild(document.createElement("th"));
+for (let romajiVowel of ROMAJI_VOWELS) {
+  const th = document.createElement("th");
+  th.innerHTML = `${romajiVowel} <span lang="ko">(${VOWEL_MAP[romajiVowel]})</span>`;
+  vowelsTr.appendChild(th);
+}
+table.appendChild(vowelsTr);
+for (let i = 0; i < ROMAJI_CONSONANTS.length; i++) {
+  const tr = document.createElement("tr");
+  const romajiConsonant = ROMAJI_CONSONANTS[i];
+  const hangulConsonant = CONSONANT_MAP[romajiConsonant];
+  const th = document.createElement("th");
+  tr.appendChild(th);
+  th.innerHTML = romajiConsonant
+    ? `${romajiConsonant} <span lang="ko">(${CONSONANT_MAP[romajiConsonant]})</span>`
+    : "";
+  for (let j = 0; j < ROMAJI_VOWELS.length; j++) {
+    const romajiVowel = ROMAJI_VOWELS[j];
+    let hangulVowel = VOWEL_MAP[romajiVowel];
+    if (romajiConsonant == "y") {
+      hangulVowel = Y_MAP[romajiVowel];
+    } else if (romajiConsonant == "w") {
+      hangulVowel = W_MAP[romajiVowel];
+    }
+    const katakana = KATAKANA[i][j];
+    const td = document.createElement("td");
+    td.innerHTML = katakana
+      ? `${katakana} <span lang="ko">(${combineSyllables(hangulConsonant, hangulVowel)})</span>`
+      : "";
+    tr.appendChild(td);
+  }
+  table.appendChild(tr);
+}
+parent.appendChild(table);
